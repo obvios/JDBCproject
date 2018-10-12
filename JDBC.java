ceba72427333e7972357f6c7b68ad7f37b436ea4
Eric Palma <palmae95@gmail.com>
2018-10-11 18:07:58 -0700

    add java file

--- JDBC.java
+++ JDBC.java
@@ -1,21 +1,71 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jdbc;
+import java.sql.*;
 
-/**
- *
- * @author ericpalma
- */
 public class JDBC {
+    static final String JDBC_DRIVER = "org.apache.derby.jdbc.ClientDriver";
+    static final String DB_URL = "jdbc:derby://localhost:1527/JDBCdatabase";
 
-    /**
-     * @param args the command line arguments
-     */
+    //  Database credentials
+    static final String USER = "user1";
+    static final String PASS = "pass1";
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Connection conn = null;
+        Statement stmt = null;
+        
+        try{
+            //STEP 1: Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            //STEP 2: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+            //STEP 3: Execute a query
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT au_id, au_fname, au_lname, phone FROM Authors";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            //STEP 4: Extract data from result set
+            while(rs.next()){
+                //Retrieve by column name
+                String id  = rs.getString("au_id");
+                String phone = rs.getString("phone");
+                String first = rs.getString("au_fname");
+                String last = rs.getString("au_lname");
+
+                //Display values
+                System.out.print("ID: " + id);
+                System.out.print(", First: " + first);
+                System.out.print(", Last: " + last);
+                System.out.println(", Phone: " + phone);
+            }
+            //STEP 5: Clean-up environment
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                stmt.close();
+            }catch(SQLException se2){}
+            
+            try{
+                if(conn!=null)
+                conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+        
     }
     
 }
